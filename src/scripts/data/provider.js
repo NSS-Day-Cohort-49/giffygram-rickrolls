ad75e69ce2eb493b5995cfd15e73739689e2e312
tbronny <tristan.bron7@gmail.com>
2021-06-28 11:41:03 -0500

    it works

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -1,12 +1,57 @@
-const apiURL = "http://localhost:8088"
-const applicationElement = document.querySelector(".giffygram")
+const API = "http://localhost:8088"
 
+const applicationElement = document.querySelector("#giffyGram")
 
 const applicationState = {
+    users: [],
+    posts: [],
+    likes: [],
+    messages: [],
     currentUser: {},
     feed: {
         chosenUser: null,
         displayFavorites: false,
-        displayMessages: false
-    }
+        displayMessages: false,
+    },
 }
+
+export const fetchUsers = () => {
+    return fetch(`${API}/users`)
+        .then((response) => response.json())
+        .then((serviceUsers) => {
+            // Store the external state in application state
+            applicationState.users = serviceUsers
+        })
+}
+
+export const fetchPosts = () => {
+    return fetch(`${API}/posts`)
+        .then((response) => response.json())
+        .then((servicePosts) => {
+            // Store the external state in application state
+            applicationState.posts = servicePosts
+        })
+}
+
+export const fetchLikes = () => {
+    return fetch(`${API}/likes`)
+        .then((response) => response.json())
+        .then((serviceLikes) => {
+            // Store the external state in application state
+            applicationState.likes = serviceLikes
+        })
+}
+
+export const fetchMessages = () => {
+    return fetch(`${API}/messages`)
+        .then((response) => response.json())
+        .then((serviceMessages) => {
+            // Store the external state in application state
+            applicationState.messages = serviceMessages
+        })
+}
+
+export const getUsers = () => [...applicationState.users]
+export const getPosts = () => [...applicationState.posts]
+export const getLikes = () => [...applicationState.likes]
+export const getMessages = () => [...applicationState.messages]
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,32 @@
 import { GiffyGram } from "./GiffyGram.js"
+import { LoginForm } from "./auth/Login.js"
+import {
+    fetchLikes,
+    fetchMessages,
+    fetchPosts,
+    fetchUsers,
+} from "./data/provider.js"
 
-const applicationElement = document.querySelector(".giffygram")
+const applicationElement = document.querySelector("#giffyGram")
 
-export const renderApp = () => {
+const renderApp = () => {
     const user = parseInt(localStorage.getItem("gg_user"))
 
-    if (user) {
-        applicationElement.innerHTML = GiffyGram()
-    } else {
-        applicationElement.innerHTML = LoginForm()
-    }
+    fetchUsers()
+        .then(fetchPosts)
+        .then(fetchLikes)
+        .then(fetchMessages)
+        .then(() => {
+            if (user) {
+                applicationElement.innerHTML = GiffyGram()
+            } else {
+                applicationElement.innerHTML = LoginForm()
+            }
+        })
 }
+
+renderApp()
+
+// applicationElement.addEventListener("stateChanged", (customEvent) => {
+//     renderApp()
+// })
