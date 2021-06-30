1559cc811606c758088a6a5a1a47be1753767bba
tbronny <tristan.bron7@gmail.com>
2021-06-30 13:19:45 -0500

    posts by user is now connected with posts userId

--- src/scripts/nav/Footer.js
+++ src/scripts/nav/Footer.js
@@ -7,20 +7,20 @@ export const Footer = () => {
     for (const post of posts) {
         return `
             <footer class="footer">
-                    <div class="footer__item">
-                        Posts by user <select id="userSelection">
-                        <option value="">Choose</option>
-                        ${users
-                            .map((user) => {
-                                return `<option value="${post.id}--${user.id}">${user.name}</option>`
-                            })
-                            .join("")}                 
-                        </select>
-                    </div>
-                    <div class="footer__item">
+                <div class="footer__item">
+                    Posts by user <select id="userSelection">
+                    <option value="">Choose</option>
+                    ${users
+                        .map((user) => {
+                            return `<option value="${post.id}--${user.id}">${user.name}</option>`
+                        })
+                        .join("")}                 
+                    </select>
+                </div>
+                <div class="footer__item">
                     Show only favorites <input id="showOnlyFavorites" type="checkbox">
-                    </div>
-                </footer>
+                </div>
+            </footer>
             `
     }
 }
