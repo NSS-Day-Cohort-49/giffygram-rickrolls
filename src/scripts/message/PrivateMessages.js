546f0bbf1a94bf03b36dc9c6b49e8583f0cedf05
IanJames2 <ianjms81@gmail.com>
2021-07-01 14:46:01 -0500

    Testing

--- src/scripts/message/PrivateMessages.js
+++ src/scripts/message/PrivateMessages.js
@@ -6,7 +6,9 @@ export const PrivateMessage = () => {
         <div class="messages">
             <div class="messageList">
                 <div class="message" id="message--21">
-                    <div class="message__author">From Ray Medrano</div>
+                    <div class="message__author">
+                    
+                    </div>
                     <div class="message__text">this is a message</div>
                 </div>
             </div>
