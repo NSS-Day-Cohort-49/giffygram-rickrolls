3a39228b3fd793d333463041cf6587eecc06a4fd
tbronny <tristan.bron7@gmail.com>
2021-07-01 13:50:14 -0500

    messages page displays

--- src/scripts/message/PrivateMessages.js
+++ src/scripts/message/PrivateMessages.js
@@ -2,53 +2,15 @@ import { getUsers, getMessages } from "../data/provider.js"
 
 
 export const PrivateMessage = () => {
-    const messages = getMessages()
-    const users = getUsers()
-
-    /*
-    ◾ If there are new messages sent to a user
-        ◾ Than display the messages
-    ◾ If the message has already been read 
-        ◾ than remove the messages
-    ◾ 
-    ◾◾◾◾◾
-    */
-
-    let html = '<ul>'
-
-    const messageItems = messages.map(messages => {
-
-
-
-        const findUser = users.find((user)=> {
-            if (messages.userId === user.id) {
-                return true
-            }})
-
-        let foundUser = findUser.name
-
-
-        
-        return `
-    <div class="message">
-            ${messages.text}
-        </div>
+    return `
         <div class="messages">
-            <a href="#" class="profileLink" id="profile--${messages.userId}">
-                ${foundUser}
-            </a>
-            on ${messages.timestamp}
-        </div>
-        <div class="message__author">
-            <div>
-                ${messages.recipientId}
+            <div class="messageList">
+                <div class="message" id="message--21">
+                    <div class="message__author">From Ray Medrano</div>
+                    <div class="message__text">this is a message</div>
+                </div>
             </div>
-    </div>`
-    })
-
-  html += messageItems.join("")
-  html += '<ul>'
-
-  return html
+        </div>
+        `
 }
 
